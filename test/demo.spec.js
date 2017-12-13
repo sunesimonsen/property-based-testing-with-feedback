c0cfbd327558566e45ef6483f7645ea7355441ee
Sune Simonsen <sune@we-knowhow.dk>
2017-12-13 23:06:47 +0100

    Initial commit

---
+++ test/demo.spec.js
@@ -0,0 +1,37 @@
+/*global describe, it, beforeEach*/
+const Generators = require('chance-generators');
+const expect = require('unexpected');
+
+const demo = require('../lib/demo');
+
+expect.use(require('unexpected-check'));
+
+const { natural, string } = new Generators(42);
+
+describe('unexpected-check', () => {
+    it('does not find the secret characters', () => {
+        expect((text) => {
+            demo.findTheSecretCharacters(text);
+        }, 'to be valid for all', string({ length: natural({ max: 50 }) }));
+    });
+
+    it('does not find the secret word', () => {
+        expect((text) => {
+            demo.findTheSecretWord(text);
+        }, 'to be valid for all', string({ length: natural({ max: 50 }) }));
+    });
+
+    it('does not find the secret prefix', () => {
+        expect((text) => {
+            demo.findTheSecretPrefix(text);
+        }, 'to be valid for all', string({ length: natural({ max: 50 }) }));
+    });
+
+    it('does not find magic numbers', () => {
+        const numbers = natural({ max: 200 });
+
+        expect((a, b, c) => {
+            demo.findTheMagicNumbers(a, b, c);
+        }, 'to be valid for all', numbers, numbers, numbers);
+    });
+});
